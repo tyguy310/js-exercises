4aa37d96c8d7d56573868fa769d2e5817001969e
Tyler Maier <tyler_maier@yahoo.com>
2016-07-11 20:35:57 -0600

    Finished #1-#7

--- js_functions.js
+++ js_functions.js
@@ -31,24 +31,28 @@
 //
 // // 3.
 // function discountPercentage(origAmt, discount)  {
-//   if (discount<100 && discount>0) {
-//     return origAmt*(discount/100);
+//   if (discount < 100 && discount > 0) {
+//     return origAmt * (discount/100);
 //   } else{
-//     return "warning"
+//     return "warning";
 //   }
 // }
 //   console.log(discountPercentage(143,15))
 //   console.log(discountPercentage(143,0))
 //
 // // 4.
-// function stringCapitalize(string) {
-//   string.toLowerCase
+// function capFirst(str) {
+//   str = str.toLowerCase().split(' ');
+//   for (var i = 0; i < str.length; i++) {
+//     str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+//   }
+//   return str.join(' ');
 // }
-
+// console.log(capFirst(""));
 
 // // 5.
 // function evenNumbers(integer) {
-//   for(i=0; i<integer; i+=2)  {
+//   for(i = 0; i < integer; i += 2)  {
 //      console.log(i);
 //   }
 // }
@@ -57,35 +61,46 @@
 
 // // 6.
 // function isDivisible(int1, int2)  {
-//   if(int1%int2 === 0)  {
+//   if( int2 === 0) {
+//     return "Nice try";
+//   } else if (int1 > 100 || int1 < 0)  {
+//     return "Enter numbers between 0 and 100"
+//   }
+//   else if (int2 > 100 || int2 < 0)  {
+//     return "Enter numbers between 0 and 100"
+//   }
+//   else if(int1 % int2 === 0)  {
 //     return true;
 //   } else {
 //     return false;
 //   }
 // }
 // console.log(isDivisible(42,5));
+// console.log(isDivisible(42,6));
+// console.log(isDivisible(42,0));
+// console.log(isDivisible(105,4));
 
-// 7.
 
-function oddNumbers(digit) {
-
-  if (digit > 100 || digit < 0) {
-    console.log("Enter a number between 0 and 100");
-  }
-
-  else if (digit < 40 && digit >= 0) {
-    for(i = 1; i < 40; i+=2)  {
-      console.log(i);
-    }
-  }
-
-  else {
-    for(j = 41; j < digit; j+=2)  {
-      console.log(j);
-    }
-  }
-}
-
-oddNumbers(-1);
-oddNumbers(67);
-oddNumbers(25);
+// // 7.
+// function oddNumbers(digit) {
+//
+//   if (digit > 100 || digit < 0) {
+//     console.log("Enter a number between 0 and 100");
+//   }
+//
+//   else if (digit < 40 && digit >= 0) {
+//     for(i = 1; i < 40; i += 2)  {
+//       console.log(i);
+//     }
+//   }
+//
+//   else {
+//     for(j = 41; j < digit; j += 2)  {
+//       console.log(j);
+//     }
+//   }
+// }
+//
+// oddNumbers(-1);
+// oddNumbers(67);
+// oddNumbers(25);
